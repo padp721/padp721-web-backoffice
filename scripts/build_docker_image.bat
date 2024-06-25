8444c5da27bdb02048866679461a183ce34d3a47
padp721 <anggadp91@gmail.com>
2024-06-26 01:41:13 +0800

    add build script for linux

--- scripts/build_docker_image.bat
+++ scripts/build_docker_image.bat
@@ -3,6 +3,6 @@
 set version="1.0.0"
 set image_name="padp721-web-backoffice"
 
-npm run build &&^
+yarn build &&^
 docker build --no-cache -t padp721/%image_name%:%version% . &&^
-docker save -o %image_name%-%version%.tar padp721/%image_name%:%version%
+docker save -o .\docker\%image_name%-%version%.tar padp721/%image_name%:%version%
